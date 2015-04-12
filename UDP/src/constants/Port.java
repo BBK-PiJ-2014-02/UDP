0dd9205c316bf994c2a63ae6506901a8cbed20bd
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-12 18:49:11 +0100

    Constants Port values.

---
+++ UDP/src/constants/Port.java
@@ -0,0 +1,24 @@
+package constants;
+/**
+ * The list of all ports to be used.
+ * 
+ * @author Vasco
+ *
+ */
+public final class Port {
+	/**
+	 * The Server TCP port.
+	 */
+	public static final int SERVER = 9876;
+
+	/**
+	 * The Server-Handler UDP port for data relay.
+	 */
+	public static final int SERVER_RELAY_UDP = 24567;
+	
+	/**
+	 * The broadcast UDP port.
+	 */
+	public static final int UDP_BROADCAST = 4567;
+	
+}
