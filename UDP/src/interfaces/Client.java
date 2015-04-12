7578de2f7335c01176d2e2112952d8500adcef98
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-12 23:54:35 +0100

    Client extends Runnable

--- UDP/src/interfaces/Client.java
+++ UDP/src/interfaces/Client.java
@@ -1,13 +1,12 @@
 package interfaces;
 /**
- * Client connects to Server via TCP.
- * Receives an unique ID from Server, and indication 
- * if a receiver or sender of UDP packets.
- * Opens an UDP connection and starts either receiving 
- * or sending UDP packets.
+ * Client connects to Server via TCP, then requests an unique ID, a role
+ * and also a Socket to start receiving or sending UDP packets from/to.
+ * 
+ * The Role can be changed via a TCP request from the Server.
  * 
  * @author Vasco
  *
  */
-public interface Client {
+public interface Client extends Runnable {
 }
