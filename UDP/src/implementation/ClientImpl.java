a9832373a1d73a8c6b1ea9534d2292cc4c0d0dd4
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-13 22:07:17 +0100

    Client Implementation. - Making space for the different roles to happen - Add finalisations() - Listen to role changes from the Handler checkTCPServer()

--- UDP/src/implementation/ClientImpl.java
+++ UDP/src/implementation/ClientImpl.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.UUID;
 
 import constants.Message;
 import constants.Port;
+import constants.Role;
 
 /**
  * The Client implementation.
@@ -20,19 +20,19 @@ import constants.Port;
  *
  */
 public class ClientImpl implements Client {
-	/**
-	 * The client Socket.
-	 */
-	private Socket clientSocket;
-	
-	/**
-	 * Data output Stream.
-	 */
-	private DataOutputStream outToServer;
-	
-	/**
-	 * Buffer Reader for input Stream.
-	 */
+    /**
+     * The client Socket.
+     */
+    private Socket clientSocket;
+    
+    /**
+     * Data output Stream.
+     */
+    private DataOutputStream outToServer;
+    
+    /**
+     * Buffer Reader for input Stream.
+     */
     private BufferedReader inFromServer;
 
     /**
@@ -45,118 +45,177 @@ public class ClientImpl implements Client {
      */
     private String id;
     
-	/**
-	 * The Main.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Client client = new ClientImpl();
-		client.run();
-	}
-	
-	/**
-	 * The Client constructor.
-	 */
-	public ClientImpl() {
-		try { 
-			clientSocket = new Socket(InetAddress.getLocalHost(),Port.SERVER);
-		} 
-		catch (IOException e) { 
-			e.printStackTrace(); 
-		}
+    /**
+     * The Main.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Client client = new ClientImpl();
+        client.run();
+    }
+    
+    /**
+     * The Client constructor.
+     */
+    public ClientImpl() {
+        try { 
+            clientSocket = new Socket(InetAddress.getLocalHost(),Port.SERVER);
+        } 
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        }
 
-		try {
-			outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		} 
-		catch (IOException e) { 
-			e.printStackTrace();
-		}
+        try {
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        } 
+        catch (IOException e) { 
+            e.printStackTrace();
+        }
 
-		try {
-			inFromServer = new BufferedReader(new InputStreamReader( clientSocket.getInputStream() )); 
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            inFromServer = new BufferedReader(new InputStreamReader( clientSocket.getInputStream() )); 
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    @Override
+    public void run() {
+        // TODO: Need to save into file or upload from file: Which file?
 
-	@Override
-	public void run() {
-		// TODO: Need to save into file or upload from file: Which file?
-		
-		// Request Role
-		while (!requestRole());
-		
-		// Request Id
-		while (!requestId());
+        // Request Role
+        while (!requestRole());
+        
+        // Request Id
+        while (!requestId());
+        
+        // Connect to the UDP socket
+
+        // Start working on the given role.
+        while(true) {
+            // Check if anything to read from Server
+            checkTCPServer();
+            
+            // Depending on current role, the action.
+
+            if (role.equals(Role.SENDER)) {
+                System.out.println("Sending...");
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (role.equals(Role.RECEIVER)) {
+                System.out.println("Receiving...");
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (role.equals(Role.SHUTDOWN)) break;
+        }
+        
+        System.out.println("Exiting");
 
 try {
-	Thread.sleep(10000L);
+    Thread.sleep(1000L);
 } catch (InterruptedException e) {
-	e.printStackTrace();
+    e.printStackTrace();
 }
-        try { clientSocket.close();} 
-        catch (IOException e) {e.printStackTrace();}
-        System.out.println("Socket closed.");
-	}
 
-	/**
-	 * Request the Id from the server.
-	 * 
-	 * @return true if successful
-	 */
-	private boolean requestId() {
-	    sendTCPMessage(Message.REQUEST_ID);
-	    id = getTCPResponse();
+        // Require finalisations. 
+        finalisations();
+    }
+
+    /**
+     * Check if the Server has any role change request.
+     */
+    private void checkTCPServer() {
+        try {
+            if ( inFromServer.ready() ) {
+                role = inFromServer.readLine();
+                System.out.println("Got new role: "+role);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Closing all loose ends.
+     */
+    private void finalisations() {
+        // Close the client Socket.
+        try { 
+            clientSocket.close();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    /**
+     * Request the Id from the server.
+     * 
+     * @return true if successful
+     */
+    private boolean requestId() {
+        sendTCPMessage(Message.REQUEST_ID);
+        id = getTCPResponse();
         sendTCPMessage(Message.SUCCESS);
         if ( id == null ) return false;
         return true;
-	}
-	
-	/**
-	 * Request Role from the server.
-	 * 
-	 * @return true if successful
-	 */
-	private boolean requestRole() {
-	    sendTCPMessage(Message.REQUEST_ROLE);
-	    role = getTCPResponse();
+    }
+    
+    /**
+     * Request Role from the server.
+     * 
+     * @return true if successful
+     */
+    private boolean requestRole() {
+        sendTCPMessage(Message.REQUEST_ROLE);
+        role = getTCPResponse();
         sendTCPMessage(Message.SUCCESS);
 
         if ( role == null ) return false;
 
         return true;
-	}
-	
-	/**
-	 * Send a message to Server.
-	 * 
-	 * @param message the message
-	 */
-	private void sendTCPMessage(String message) {
-		if ( message == null ) throw new IllegalArgumentException("Cannot send a null message.");
-		try {
-			outToServer.writeBytes(message+"\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    }
+    
+    /**
+     * Send a message to Server.
+     * 
+     * @param message the message
+     */
+    private void sendTCPMessage(String message) {
+        if ( message == null ) throw new IllegalArgumentException("Cannot send a null message.");
+        try {
+            outToServer.writeBytes(message+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Get a response from the server.
-	 * 
-	 * @return server String response
-	 */
-	private String getTCPResponse() {
-		String message = null;
-		try { 
-	    	message = inFromServer.readLine(); 
-	    }
-	    catch (IOException e) {
-	    	e.printStackTrace();
-		}
-		
-		return message;
-	}
+    /**
+     * Get a response from the server.
+     * 
+     * @return server String response
+     */
+    private String getTCPResponse() {
+        String message = null;
+        try { 
+            message = inFromServer.readLine(); 
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return message;
+    }
 }
