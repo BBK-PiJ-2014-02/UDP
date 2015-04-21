d71042058e4742110b2f2b2b569da00f4d9e75a0
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-21 20:18:57 +0100

    Constants Message Add sending and receiving udp port request messages

--- UDP/src/constants/Message.java
+++ UDP/src/constants/Message.java
@@ -6,11 +6,13 @@ package constants;
  *
  */
 public class Message {
-    public static final String REQUEST_ROLE = "REQUEST_ROLE";   // Sent by the client requesting the role to play
-    public static final String RESPONSE_ROLE = "RESPONSE_ROLE"; // The Role response
-    public static final String REQUEST_ID = "REQUEST_ID";       // The Client ID requested to the server
-    public static final String RESPONSE_ID = "RESPONSE_ID";     // The Id response
-    public static final String RETRY = "RETRY";                 // Retry message on last action
-    public static final String ERROR = "ERROR";                 // Default message for error
-    public static final String SUCCESS = "SUCCESS";             // Successfully processed message 
+    public static final String REQUEST_ROLE = "REQUEST_ROLE";                 // Sent by the client requesting the role to play
+    public static final String RESPONSE_ROLE = "RESPONSE_ROLE";               // The Role response
+    public static final String REQUEST_ID = "REQUEST_ID";                     // The Client ID requested to the server
+    public static final String RESPONSE_ID = "RESPONSE_ID";                   // The Id response
+    public static final String RETRY = "RETRY";                               // Retry message on last action
+    public static final String ERROR = "ERROR";                               // Default message for error
+    public static final String SUCCESS = "SUCCESS";                           // Successfully processed message 
+    public static final String REQUEST_SENDING_UDP_PORT = "SENDING_PORT";     // Requested by the Handler to Client 
+    public static final String REQUEST_RECEIVING_UDP_PORT = "RECEIVING_PORT"; // Requested by Client to the Handler 
 }
