90dfbabf9cf359888bdfb67878414d537771423e
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-25 16:59:17 +0100

    Packet constant - Add the CHUNK_SIZE 1024 - Increased for now the PACKET_SIZE to 2048

--- UDP/src/constants/Packet.java
+++ UDP/src/constants/Packet.java
@@ -4,5 +4,10 @@ public class Packet {
 	/**
 	 *  The max size for each packet between Server and Client.
 	 */
-    public static final int PACKET_SIZE = 1024;
+    public static final int PACKET_SIZE = 2048;
+
+    /**
+     * The actual data size.
+     */
+    public static final int CHUNK_SIZE = 1024;
 }
