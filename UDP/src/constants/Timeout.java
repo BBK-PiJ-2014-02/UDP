21962f03aa4a4f22344dcbc7e1ddb23c729d891b
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-26 18:17:02 +0100

    Timeout constant - setting up different time outs for udp sockets, tcp sockets and for the server socket.

--- UDP/src/constants/Timeout.java
+++ UDP/src/constants/Timeout.java
@@ -1,5 +1,7 @@
 package constants;
 
 public class Timeout {
-    public static int TIMEOUT_DELAY = 1000; // The global timeout for any waiting.
+    public static int UDP_SOCKET_TIMEOUT_DELAY    = 1000;  // The UDP global socket timeout.
+    public static int TCP_SOCKET_TIMEOUT_DELAY    = 5000;  // The TCP global socket timeout.
+    public static int SERVER_SOCKET_TIMEOUT_DELAY = 10000; // The time after which the Server gives up waiting for new client to connect.
 }
