a1ad04c797bc42a5adc6e6ee6bcff327644354fc
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-26 19:01:50 +0100

    TCPServerImpl converting tabs to spaces

--- UDP/src/implementation/TCPServerListener.java
+++ UDP/src/implementation/TCPServerListener.java
@@ -9,44 +9,44 @@ import java.io.IOException;
  *
  */
 public class TCPServerListener implements Runnable {
-	/**
-	 * The ServerClientHandlerImpl hander.
-	 */
-	private ServerClientHandlerImpl handler;
-	
-	/**
-	 * The Constructor.
-	 * 
-	 * @param server 
-	 */
-	public TCPServerListener(ServerClientHandlerImpl server) {
-		this.handler = server;
-	}
+    /**
+     * The ServerClientHandlerImpl hander.
+     */
+    private ServerClientHandlerImpl handler;
+    
+    /**
+     * The Constructor.
+     * 
+     * @param server 
+     */
+    public TCPServerListener(ServerClientHandlerImpl server) {
+        this.handler = server;
+    }
 
-	/**
-	 * Runnable
-	 */
-	@Override
-	public void run() {
-		String message = "";
+    /**
+     * Runnable
+     */
+    @Override
+    public void run() {
+        String message = "";
 
-		// Check if shutdown signal was issued.
-		while(handler.clientTCPSocket.isConnected()) {
-			try {
-				// Waits until a message is received.
-				message = handler.inputStream.readLine();
+        // Check if shutdown signal was issued.
+        while(handler.clientTCPSocket.isConnected()) {
+            try {
+                // Waits until a message is received.
+                message = handler.inputStream.readLine();
 
-				// A null message only means the socket closed.
-				if (message == null) break;
+                // A null message only means the socket closed.
+                if (message == null) break;
 
-				// Relays the message to the hander to be processed.
-				handler.receivingTCPMessage(message);
+                // Relays the message to the hander to be processed.
+                handler.receivingTCPMessage(message);
 
-			} catch (IOException e) {
-	            e.printStackTrace();
-	            break;
-	        }
-		}
-		System.out.println("SERVER Listener closed.");
-	}
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+        System.out.println("SERVER Listener closed.");
+    }
 }
