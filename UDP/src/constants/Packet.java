189d12a801634204512ffb038066a9968ce33424
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-26 18:15:21 +0100

    Packet constant - Set packet size to be the chunk size plus 256. Note: this needs to be increased if the PacketData extends functionality

--- UDP/src/constants/Packet.java
+++ UDP/src/constants/Packet.java
@@ -1,13 +1,15 @@
 package constants;
 
 public class Packet {
-	/**
-	 *  The max size for each packet between Server and Client.
-	 */
-    public static final int PACKET_SIZE = 2048;
-
     /**
      * The actual data size.
      */
     public static final int CHUNK_SIZE = 1024;
+
+    /**
+	 *  The max size for each packet between Server and Client.
+	 *  Size of this will be for the serialized PacketData which
+	 *  will include one chunck size.
+	 */
+    public static final int PACKET_SIZE = CHUNK_SIZE + 256;
 }
