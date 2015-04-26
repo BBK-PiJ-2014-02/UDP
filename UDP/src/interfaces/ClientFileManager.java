c966f863eb269fbe218821d6f3463adce9f45e3e
Vasco Horta <vdiasf01@dcs.bbk.ac.uk>
2015-04-26 14:39:45 +0100

    FileManager static interface - Adding saveFile() to save packet lists into file on the receive bucket, keeping original name.

--- UDP/src/interfaces/ClientFileManager.java
+++ UDP/src/interfaces/ClientFileManager.java
@@ -18,13 +18,13 @@ public interface ClientFileManager {
      * @return true if more files
      */
     public boolean hasMoreFilesToSend();
-    
+
     /**
      * Load the next file from pool to send,
      * and prepares it to be processed in chunks.
      */
     public void loadNextFileToSend();
-    
+
     /**
      * Get the next chunk converted into a ready-to-send PackageData.
      * 
@@ -39,4 +39,31 @@ public interface ClientFileManager {
      * @return true if EOF
      */
     public boolean isEOF();
+
+    /**
+     * Returns true if Client is sending a file.
+     * 
+     * @return true if sending
+     */
+    public boolean isSending();
+
+    /**
+     * Saves packet data into file if all packets have been received in 
+     * sequence from packet 0 to current, and will close file whenever
+     * the last packet is received.
+     * 
+     * @param packetData
+     */
+    public void savePacket(PacketData packetData);
+
+    /**
+     * Whenever the Server decides to change the role of the client,
+     * the client should change to receive a file instead of continuing
+     * sending the current. 
+     * 
+     * Also this could be used by the Client when a file has been finished,
+     * or automatically by the ClientFileManager when no more files exist
+     * to be sent.
+     */
+    public void setToReceive();
 }
--- UDP/src/interfaces/FileManager.java
+++ UDP/src/interfaces/FileManager.java
@@ -1,6 +1,8 @@
 package interfaces;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -30,7 +32,7 @@ public interface FileManager {
 
         List<File> fileListInBucket = new LinkedList<File>();
 
-        // Pull all test file from the default directory.
+        // Pull all files from given bucket directory.
         DirectoryStream<Path> stream = null;
         try {
             stream = Files.newDirectoryStream(dir);
@@ -50,4 +52,66 @@ public interface FileManager {
 
         return fileListInBucket;
     }
+
+    /**
+     * From a list of packets, saves the data of each into given file.
+     * 
+     * @param packetList the list of packets
+     * @param file the to store info into
+     */
+    public static void saveFile(List<PacketData> packetList) {
+        // Dealing with all exceptions.
+        if ( packetList == null ) throw new IllegalArgumentException("Cannot save a null packet list to file.");
+        if ( packetList.size() == 0 ) throw new IllegalArgumentException("Cannot save an emtpy packet list to file.");
+
+        // Get the original path and file names
+        String originalPathFile = packetList.get(0).getUID();
+
+        // Convert it into a File type object
+        File originalFile = new File(originalPathFile);
+
+        // Collect the original path, move up to parent twice such that:
+        // ../something/send/file.txt becomes ../something/receive/file.txt 
+        Path originalPath = originalFile.toPath().getParent().getParent();
+        String fullPath = originalPath.toString() + File.separatorChar + "receive" + File.separatorChar;
+
+        // Collect only the file name.
+        String originalFileName = originalFile.getName();
+
+        // Append the file name to use to the calculated path
+        File file = new File(fullPath + originalFileName);
+
+        // Create the file if it does not exists.
+        if ( !file.exists() ) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for( PacketData packet : packetList ) {
+            byte[] data = packet.getData();
+            try {
+                fileOutputStream.write(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    };
 }
